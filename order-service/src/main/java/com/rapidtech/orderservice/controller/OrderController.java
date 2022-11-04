b80cc7f17b877f46c17b66d60c7bed3784990d03
mhrifai <mkhrifai99@gmail.com>
2022-11-04 15:01:01 +0700

    circuit breaker

--- order-service/src/main/java/com/rapidtech/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/rapidtech/orderservice/controller/OrderController.java
@@ -4,9 +4,14 @@ import com.rapidtech.orderservice.dto.OrderDto;
 import com.rapidtech.orderservice.dto.OrderReq;
 import com.rapidtech.orderservice.dto.OrderResDto;
 import com.rapidtech.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,9 +21,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory")
     public String placeOrder(@RequestBody OrderReq orderReq){
         orderService.placeOrder(orderReq);
         return "Produk berhasil dibeli.";
+    }
+
+    public String fallbackMethod(OrderReq orderReq){
+        return "Terjadi kesalahan, silahkan untuk order kembali beberapa saat lagi...";
     }
 
     @GetMapping("/{id}")
