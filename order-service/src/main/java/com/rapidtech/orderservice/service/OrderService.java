e261cdaac6a4a4cd522bae79c338e01dfe5720e5
mhrifai <mkhrifai99@gmail.com>
2022-11-04 10:36:57 +0700

    notification services kafka

--- order-service/src/main/java/com/rapidtech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rapidtech/orderservice/service/OrderService.java
@@ -1,12 +1,14 @@
 package com.rapidtech.orderservice.service;
 
 import com.rapidtech.orderservice.dto.*;
+import com.rapidtech.orderservice.event.OrderPlacedEvent;
 import com.rapidtech.orderservice.model.Order;
 import com.rapidtech.orderservice.model.OrderLineItems;
 import com.rapidtech.orderservice.repository.OrderLineItemsRepo;
 import com.rapidtech.orderservice.repository.OrderRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,8 +22,9 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepo orderRepo;
-    private final OrderLineItemsRepo orderLineItemsRepo;
     private final WebClient.Builder webClientBuilder;
+
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public void placeOrder(OrderReq orderReq){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -35,6 +38,7 @@ public class OrderService {
                 .map(OrderLineItems::getPrice).toList();
         List<Integer> r = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getQuantity).toList();
+        log.info("Memanggil product service");
 
         //cek di bagian product-service
         ProductStockRes[] productResponsesArr = webClientBuilder.build().get().uri("http://product-services/api/product",
@@ -52,6 +56,7 @@ public class OrderService {
         if(allProductsInStock){
             List<String> userNames = order.getOrderLineItemsList().stream()
                     .map(OrderLineItems::getUserName).toList();
+            log.info("Memanggil Wallet service");
             //cek ballance di wallet
             WalletBalanceRes[] walletBalanceResArr = webClientBuilder.build().get().uri("http://wallet-service/api/wallet",
                             uriBuilder -> uriBuilder.queryParam("userName", userNames)
@@ -62,6 +67,7 @@ public class OrderService {
             boolean saldoAvailable = Arrays.stream(walletBalanceResArr).allMatch(WalletBalanceRes::isSaldoAvail);
             if(saldoAvailable){
                 orderRepo.save(order);
+                kafkaTemplate.send("notificationTopic",new OrderPlacedEvent(order.getOrderNumber()));
             }else {
                 throw new IllegalArgumentException("Saldo Anda tidak cukup....");
             }
